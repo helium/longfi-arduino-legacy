7483ccbcf944ce47ecb75fcc3e9be70979b3f23b
Kent Williams <k3nt00@gmail.com>
2019-09-26 15:23:06 -0700

    working discovery sketch

--- examples/TransmitPacket/TransmitPacket.ino
+++ examples/TransmitPacket/TransmitPacket.ino
@@ -6,19 +6,38 @@
 const uint32_t oui = 1234;
 const uint16_t device_id = 99;
 
-// provide chip select, DIO0/INT, and RESET for any board
-const uint8_t RFM95_CS = 8;
-const uint8_t RFM95_INT = 3;
-const uint8_t RFM95_RST = 4;
+// Discovery L072CZ-LRWAN1
+// const uint8_t RADIO_RESET_PIN = RADIO_RESET_PORT;
+// const uint8_t RADIO_TCXO_PIN  = RADIO_TCXO_PORT; // Do not enable
+// const uint8_t RADIO_DIO_0_PIN = RADIO_DIO_0_PORT;
+// const uint8_t RADIO_MOSI_PIN  = RADIO_MOSI_PORT;
+// const uint8_t RADIO_MISO_PIN  = RADIO_MISO_PORT;
+// const uint8_t RADIO_SS_PIN    = RADIO_NSS_PORT;
 
-LongFi LongFi(LongFi::RadioType::SX1276, RFM95_RST, RFM95_CS, RFM95_INT);
+// MCCI Catena 4610
+const uint8_t RADIO_RESET_PIN = RADIO_RESET;
+const uint8_t RADIO_TCXO_PIN  = RADIO_TCXO;
+const uint8_t RADIO_DIO_0_PIN = RADIO_DIO_0;
+const uint8_t RADIO_MOSI_PIN  = RADIO_MOSI;
+const uint8_t RADIO_MISO_PIN  = RADIO_MISO;
+const uint8_t RADIO_SS_PIN    = RADIO_SS;
+
+LongFi LongFi(LongFi::RadioType::SX1276, RADIO_RESET_PIN, RADIO_SS_PIN, RADIO_DIO_0_PIN);
 
 void setup() {
   Serial.begin(9600);
   LongFi.init(oui, device_id);
+  Serial.println("Setup Start");
+
+  // Setup Pins for TCXO Enable
+  pinMode(RADIO_TCXO_PIN, OUTPUT);
+  pinMode(RADIO_RESET_PIN, OUTPUT);
+
+  // Init LongFi
+  LongFi.init(oui, device_id);
 }
 
-uint8_t data[42] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
+uint8_t data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 void loop() {
   Serial.print("sending: ");
