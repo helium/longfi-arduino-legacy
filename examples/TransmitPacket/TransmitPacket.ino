cfc6036a79bb3e790a82ed33565dc51979e55c4b
Kent Williams <k3nt00@gmail.com>
2019-09-30 15:38:23 -0700

    added #ifdef for board specific defines and tcxo enable

--- examples/TransmitPacket/TransmitPacket.ino
+++ examples/TransmitPacket/TransmitPacket.ino
@@ -6,6 +6,7 @@
 const uint32_t oui = 1234;
 const uint16_t device_id = 99;
 
+#ifdef _VARIANT_ARDUINO_STM32_
 // Discovery L072CZ-LRWAN1
 const uint8_t RADIO_RESET_PIN = RADIO_RESET_PORT;
 const uint8_t RADIO_DIO_0_PIN = RADIO_DIO_0_PORT;
@@ -13,15 +14,17 @@ const uint8_t RADIO_MOSI_PIN  = RADIO_MOSI_PORT;
 const uint8_t RADIO_MISO_PIN  = RADIO_MISO_PORT;
 const uint8_t RADIO_SCLK_PIN  = RADIO_SCLK_PORT;
 const uint8_t RADIO_SS_PIN    = RADIO_NSS_PORT;
-
+#endif
+#ifdef _VARIANT_ARDUINO_CATENA_461x_
 // MCCI Catena 4610
-// const uint8_t RADIO_RESET_PIN = RADIO_RESET;
-// const uint8_t RADIO_TCXO_PIN  = 33;
-// const uint8_t RADIO_DIO_0_PIN = 25;
-// const uint8_t RADIO_MOSI_PIN  = RADIO_MOSI;
-// const uint8_t RADIO_MISO_PIN  = RADIO_MISO;
-// const uint8_t RADIO_SCLK_PIN  = RADIO_SCK;
-// const uint8_t RADIO_SS_PIN    = RADIO_SS;
+const uint8_t RADIO_RESET_PIN = RADIO_RESET;
+const uint8_t RADIO_TCXO_PIN  = 33;
+const uint8_t RADIO_DIO_0_PIN = 25;
+const uint8_t RADIO_MOSI_PIN  = RADIO_MOSI;
+const uint8_t RADIO_MISO_PIN  = RADIO_MISO;
+const uint8_t RADIO_SCLK_PIN  = RADIO_SCK;
+const uint8_t RADIO_SS_PIN    = RADIO_SS;
+#endif
 
 LongFi LongFi(LongFi::RadioType::SX1276, RADIO_RESET_PIN, RADIO_SS_PIN, RADIO_DIO_0_PIN);
 
@@ -37,11 +40,13 @@ void setup() {
   LongFi.init(oui, device_id);
   Serial.println("Setup Start");
 
-  // ***IMPORTANT: Only For Catena 4610
-  //LongFi.enable_tcxo(RADIO_TCXO_PIN);
+  #ifdef _VARIANT_ARDUINO_CATENA_461x_
+  // Enable TCXO On Catena Boards 
+  LongFi.enable_tcxo(RADIO_TCXO_PIN);
+  #endif
 }
 
-uint8_t data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+uint8_t data[8] = {1, 2, 3, 4, 5, 6, 9, 9};
 
 void loop() {
   Serial.print("sending: ");
